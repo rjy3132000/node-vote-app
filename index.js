c6fb291d800eebb0bf4bd8ccdc15bd82cbd21d5c
Rajesh <rajesh.yadav@techsierra..in>
2024-12-15 17:42:15 +0530

    connect the database from clusters

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const { dbconnect } = require("./db");
 require("dotenv").config();
+
+dbconnect();
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server is running on this port: ${PORT}`));
